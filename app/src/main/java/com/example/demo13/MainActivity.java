4e1845ad34e0160e7f8093123ce900bd6347bf90
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-04-01 17:12:54 +0300

    Ex13

--- app/src/main/java/com/example/demo13/MainActivity.java
+++ app/src/main/java/com/example/demo13/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
     double num = 0, result = 0;
     String action="+";
